f48ea3a473b3c52ca92b9c18a158fa719dbf1405
Antonio Laguna <sombragriselros@gmail.com>
2017-04-28 12:44:18 +0200

    Adding library

---
+++ index.js
@@ -0,0 +1,52 @@
+export default class Metrics {
+  constructor(host, debug = false) {
+    this.host = host;
+    this.debug = debug;
+
+    if (this.host[host.length - 1] === '/') {
+      // URL ending with '/'
+      this.host = host.slice(0, -1);
+    }
+  }
+
+  track(type, value, tagsObj) {
+    const tags = Object
+      .keys(tagsObj)
+      .map(tag => `${tag}:${tagsObj[tag]}`)
+      .join(',');
+
+    const url = `${this.host}/t.gif?type=${type}&value=${value}&tags=${tags}`;
+    this.log(url);
+    const img = new Image();
+    img.src = url;
+    document.body.appendChild(img);
+  }
+
+  trackLoad() {
+    if (window.performance && window.performance.timing) {
+      // Not complete
+      if (document.readyState !== 'complete') {
+        window.addEventListener('load', () => {
+          // Load listeners have to finish for loadEventEnd to have value
+          setTimeout(this.trackLoad.bind(this));
+        });
+      } else if (!window.performance.timing.loadEventEnd) {
+        // This might happen on load event
+        setTimeout(this.trackLoad.bind(this));
+      } else {
+        // We're good to go
+        const start = window.performance.timing.fetchStart;
+        const end = window.performance.timing.loadEventEnd;
+        const value = end - start;
+        const isMobile = window.matchMedia && window.matchMedia('(max-width: 767px)').matches;
+        this.track('browser.performance', value, { isMobile });
+      }
+    }
+  }
+
+  log(...args) {
+    if (this.debug) {
+      console.log('METRICS', args); //eslint-disable-line no-console
+    }
+  }
+}
---
+++ test/micro-metrics-browser.test.js
@@ -0,0 +1,46 @@
+import test from 'tape-rollup';
+import MicroMetrics from '../index';
+
+const getImages = () => document.querySelectorAll('img');
+
+const teardown = () => {
+  const images = getImages();
+  [].forEach.call(images, img => {
+    img.remove();
+  });
+};
+
+test('should normalise host', assert => {
+  const mm = new MicroMetrics('https://localhost/');
+  assert.equal(mm.host, 'https://localhost', 'last slash is removed');
+  assert.end();
+});
+
+test('should not debug by default', assert => {
+  const mm = new MicroMetrics('https://localhost/');
+  assert.equal(mm.debug, false, 'debug is false');
+  assert.end();
+});
+
+test('image should have correct parameters', assert => {
+  const mm = new MicroMetrics('https://localhost/');
+  mm.track('foo', 100, {
+    bar: 'baz',
+    test: 'works'
+  });
+  const img = getImages()[0].src;
+
+  assert.equal(img, 'https://localhost/t.gif?type=foo&value=100&tags=bar:baz,test:works', 'data should be correct');
+  assert.end();
+  teardown();
+});
+
+test('image should have correct parameters', assert => {
+  const mm = new MicroMetrics('https://localhost/');
+  mm.trackLoad();
+
+  const img = getImages()[0].src;
+  assert.ok(img.indexOf('https://localhost/t.gif?type=browser.performance&value=') > -1, 'contains correct start');
+  assert.ok(img.indexOf('&tags=isMobile:') > -1, 'contains correct correct tags');
+  assert.end();
+});
