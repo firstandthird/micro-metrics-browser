1aa606714239aed814f2e72de891765b9a170c69
Alejandro Pérez Martín <aleperez92@gmail.com>
2017-12-01 16:41:57 +0100

    Added click track functionality

--- trackings/metrics.js
+++ trackings/metrics.js
@@ -1,3 +1,5 @@
+import { on, find, ready } from 'domassist';
+
 export default class Metrics {
   constructor(host, debug = false) {
     this.host = host;
@@ -8,6 +10,8 @@ export default class Metrics {
       // URL ending with '/'
       this.host = host.slice(0, -1);
     }
+
+    ready(this.autotrack.bind(this));
   }
 
   track(type, value, tagsObj = {}, dataObj = {}) {
@@ -42,6 +46,62 @@ export default class Metrics {
     this.tracked.push(url);
     const img = new Image();
     img.src = url;
+  }
+
+  getData(element, options = {}) {
+    const dataset = element.dataset;
+    const href = dataset.mmTrackHref || element.getAttribute('href');
+    const value = dataset.mmTrackValue || options.value;
+    const type = dataset.mmTrackType || options.type;
+    const tags = options.tags || {};
+    const data = options.data || {};
+
+    Object.keys(dataset).forEach(key => {
+      if (key.includes('mmTrackTag')) {
+        tags[key.replace('mmTrackTag', '').toLowerCase()] = dataset[key];
+      } else if (key.includes('mmTrackData')) {
+        data[key.replace('mmTrackData', '').toLowerCase()] = dataset[key];
+      }
+    });
+
+    return { type, value, tags, data, href };
+  }
+
+  trackElements(element, options = {}) {
+    if (Array.isArray(element)) {
+      element.forEach(data => {
+        find(data.element).forEach(el => {
+          this.trackElements(el, data);
+        });
+      });
+
+      return;
+    }
+
+    this.log('tracking', element, options);
+    on(element, 'click', event => {
+      this.onTrackedClick(element, event, options);
+    });
+  }
+
+  onTrackedClick(element, event, options) {
+    const data = this.getData(element, options);
+    const target = element.getAttribute('target');
+
+    this.track(data.type, data.value, data.tags, data.data);
+
+    if (element.dataset.mmTrackHref === 'false') {
+      event.preventDefault();
+    } else if (data.href && !event.metaKey && event.which === 1 && target !== '_blank') {
+      event.preventDefault();
+      setTimeout(() => { window.location = data.href; }, options.delay);
+    }
+  }
+
+  autotrack() {
+    const elements = find('[data-mm-track]');
+
+    elements.forEach(element => this.trackElements(element));
   }
 
   log(...args) {
